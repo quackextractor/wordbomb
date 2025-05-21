f468919abfecb48dd5fb9e5f15b0dfca0578249f
LostSoul <miro.slezak0601@gmail.com>
2025-05-21 16:05:56 +0200

    Critical fix

--- client/src/utils/definitionUtils.js
+++ client/src/utils/definitionUtils.js
@@ -3,6 +3,7 @@
  */
 
 let currentDefinition = null;
+let lastDefinition = null;
 
 /**
  * Get the current definition
@@ -10,4 +11,13 @@ let currentDefinition = null;
  */
 export const getDefinition = () => {
     return currentDefinition;
+};
+
+/**
+ * Set the current definition
+ * @param {Object|string} definition - The definition to set
+ */
+export const setDefinition = (definition) => {
+    lastDefinition = currentDefinition;
+    currentDefinition = definition;
 };
